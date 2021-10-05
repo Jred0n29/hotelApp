9cdaa08ec3fe35eef28477e89d09e8e26d9786a7
Jred0n29 <jredon439@gmail.com>
2021-10-04 21:38:17 -0500

    Crecencialess

--- HotelSince/settings/production.py
+++ HotelSince/settings/production.py
@@ -11,10 +11,10 @@ ALLOWED_HOSTS = ['hotelsanluis.herokuapp.com']
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hotel',
-        'USER':'postgres',
-        'PASSWORD': 'jredon29',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '5432'
+        'NAME': 'd6eplu40cdb5h7',
+        'USER':'pxmplfkbafeywi',
+        'PASSWORD': '6015cf419eb3aa0fae95f75283fd4e0c18189eb16b17de395e5822aa1f28c869',
+        'HOST': 'ec2-3-209-65-193.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
